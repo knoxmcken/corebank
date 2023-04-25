02bc6be8c5bcdd72f23fb3cf690e4806a9877046
knoxmcken <48538863+knoxmcken@users.noreply.github.com>
2023-04-25 18:29:44 +1000

    feat:create and initialize postregsql db server

---
+++ config.js
@@ -0,0 +1,13 @@
+
+
+module.exports = {
+  development: {
+    username: 'core_bank_db_user',
+    password: 'core_bank_db_pw',
+    database: 'core_bank_db',
+    host: 'core_bank_db_host',
+    dialect: 'postgres'
+  }
+  // You can also add configurations for other environments like "test" and "production"
+};
+
